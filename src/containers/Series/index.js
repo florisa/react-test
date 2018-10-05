b6e47ede14291607611fed771dafcd985c58ccff
Florisa Martins <florisa.martins@rwth-aachen.de>
2018-10-05 12:53:36 +0200

    first commit

---
+++ src/containers/Series/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Series extends Component {
+	state = {
+    series:[]
+  }
+
+/*  componentDidMount(){
+    const series = ["Vikings", "Game of thrones"]
+    setTimeout(() => {
+      this.setState({series: series});
+    }, 2000);
+  }*/
+
+  componentDidMount(){
+    fetch(' http://api.tvmaze.com/search/shows?q=Vikings')
+    /*.then((response)=>{console.log(response)})*/
+    .then(response=>response.json())
+    /*.then(json => console.log(json))*/
+    .then(json => this.setState({series: json}))
+  }
+	render() {
+		return (
+			<div> 
+			The length of series array - {this.state.series.length}
+			</div>
+		)
+	}
+}
+
+export default Series;
